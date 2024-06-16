312bc6c5c688d92006db018585c4886d88cd0575
RayyanNafees <nafees.rayyan@gmail.com>
2024-06-16 17:13:39 +0530

    feat(home): search added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -99,7 +99,7 @@ const newSurveys: Survey[][] = divideArray3(surveys);
 console.log(newSurveys);
 
 export function Home() {
-	const [search, setSearch] = useState<string>();
+	const [search, setSearch] = useState<string[]>([]);
 	return (
 		<>
 			<input
@@ -107,8 +107,14 @@ export function Home() {
 				id="search"
 				data-search
 				placeholder="Search or survey topics"
-				value={search}
-				onChange={(e) => setSearch(e.currentTarget.value)}
+				onInput={(e) => {
+					const searched = e.currentTarget.value.toLocaleLowerCase();
+					if (!searched) return setSearch([]);
+					const inSearch = surveys.filter((survey) =>
+						survey.title.toLowerCase().includes(searched),
+					);
+					setSearch(inSearch.map((survey) => survey.id));
+				}}
 			/>
 
 			<div>
@@ -116,7 +122,9 @@ export function Home() {
 					<div key={crypto.randomUUID()} class="grid">
 						{surveyArr.map((surveyItem) => (
 							<div key={surveyItem?.id}>
-								<article hidden={search && !search.includes(surveyItem?.id)}>
+								<article
+									hidden={search.length && !search.includes(surveyItem?.id)}
+								>
 									<header>{surveyItem.title}</header>
 									<ul>
 										<li>{surveyItem.description}</li>
@@ -131,114 +139,6 @@ export function Home() {
 						))}
 					</div>
 				))}
-				{/* <div class="grid">
-					<article>
-						<header>Apple iPhone</header>
-						<ul>
-							<li>Best Apple iPhone?</li>
-						</ul>
-						<footer>
-							<a role="button" href="apple.html">
-								Take Survey
-							</a>
-						</footer>
-					</article>
-					<article>
-						<header>Dell Laptops</header>
-						<ul>
-							<li>Best Dell Laptop?</li>
-						</ul>
-						<footer>
-							<a role="button" href="dell.html">
-								Take Survey
-							</a>
-						</footer>
-					</article>
-					<article>
-						<header>Cricketer</header>
-						<ul>
-							<li>Best player?</li>
-						</ul>
-						<footer>
-							<a role="button" href="cricket.html">
-								Take Survey
-							</a>
-						</footer>
-					</article>
-				</div>
-				<div class="grid">
-					<article>
-						<header>Colgate</header>
-						<ul>
-							<li>Best Type of Product?</li>
-						</ul>
-						<footer>
-							<a role="button" href="colgate.html">
-								Take Survey
-							</a>
-						</footer>
-					</article>
-					<article>
-						<header>Adidas</header>
-						<ul>
-							<li>Best Type of Adidas' Product?</li>
-						</ul>
-						<footer>
-							<a role="button" href="adidas.html">
-								Take Survey
-							</a>
-						</footer>
-					</article>
-					<article>
-						<header>Disney Streaming Service</header>
-						<ul>
-							<li>Best Disney streaming service?</li>
-						</ul>
-						<footer>
-							<a role="button" href="disney.html">
-								Take Survey
-							</a>
-						</footer>
-					</article>
-				</div>
-				<div class="grid">
-					<div>
-						<article>
-							<header>Sony Headphones</header>
-							<ul>
-								<li>Best Type of Sony's Headphones?</li>
-							</ul>
-							<footer>
-								<a role="button" href="sony.html">
-									Take Survey
-								</a>
-							</footer>
-						</article>
-					</div>
-
-					<article>
-						<header>Toyota</header>
-						<ul>
-							<li>Best Toyota Car?</li>
-						</ul>
-						<footer>
-							<a role="button" href="toyota.html">
-								Take Survey
-							</a>
-						</footer>
-					</article>
-					<article>
-						<header>Gaming Console</header>
-						<ul>
-							<li>Your favorite Gaming Console?</li>
-						</ul>
-						<footer>
-							<a role="button" href="gaming.html">
-								Take Survey
-							</a>
-						</footer>
-					</article>
-				</div> */}
 			</div>
 		</>
 	);
